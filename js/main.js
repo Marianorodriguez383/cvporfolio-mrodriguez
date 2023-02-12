57b69210300da4f9710167f75bc40686f6e4d8ee
Marianorodriguez383 <106201716+Marianorodriguez383@users.noreply.github.com>
2023-02-12 19:08:00 -0300

    se agrego visor color hex para title

--- js/main.js
+++ js/main.js
@@ -1,24 +1,50 @@
+// function cambiarFondo() {
+//     let nombre = document.getElementById('nombre');
 
-function cambiarFondo() {
-    let nombre = document.getElementById('nombre');
+//     nombre.style.backgroundColor = '#009090';
 
-    nombre.style.backgroundColor = '#009090';
+// }
 
+const $generate = document.getElementById('generate'),
+        $reset = document.getElementById('reset'),
+        $showColor = document.querySelector('.color-hex');
+
+    let hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+document.addEventListener('click', e =>{
+    if(e.target === $generate){
+        let color = "#"
+        for (let i = 0; i < 6 ; i++) {color += hex[numeroRandom()]};
+        $showColor.innerHTML = color;
+        nombre.style.backgroundColor = color;
+    }
+    if (e.target === $reset ){
+        $showColor.innerHTML = "#495670"
+        nombre.style.backgroundColor = "#495670"
+    }
+})
+
+function numeroRandom(){
+    return Math.floor(Math.random()* hex.length)
 }
+
+
+
+
+
+
 
 let hideText_btn = document.getElementById('hideText_btn');
 let hideText = document.getElementById('hideText');
 
 hideText_btn.addEventListener('click', toggleText);
 
-function toggleText(){
+function toggleText() {
     hideText.classList.toggle('show');
 
-    if (hideText.classList.contains('show')){
+    if (hideText.classList.contains('show')) {
         hideText_btn.innerHTML = "Menos Info"
-    }
-    else {
+    } else {
         hideText_btn.innerHTML = "Más Info"
     }
 }
-
